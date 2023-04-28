3d3a4992f1a9134c11787cadbf41e8e6a393f8b1
Vlamonster <jellemartijndijkstra@gmail.com>
2023-04-29 00:12:55 +0200

    sanitize comments from .rasm files.

--- src/main.rs
+++ src/main.rs
@@ -205,7 +205,7 @@ fn main() {
     let mut opcodes = vec![0; ROM_BYTES];
 
     for (i, line) in input.lines().enumerate() {
-        match parse_instr(line) {
+        match parse_instr(line.split_once("//").unwrap().0) {
             Some(v) => opcodes[i] = v,
             None => {
                 eprintln!("Line {i} does not contain a valid instruction `{line}`.");
